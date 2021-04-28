23d4da888638dc1ad5152614e602f2c8684686ba
Reuben T Allik <reubenallik@gmail.com>
2021-04-28 18:27:13 -0400

    form added

---
+++ src/components/Form.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+}
+
+const Form = () => {
+    const [state, setState] = useState({})
+    
+    const handleChange = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value })
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const form = e.target
+        fetch('/', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          body: encode({
+            'form-name': form.getAttribute('name'),
+            ...state,
+          }),
+        })
+          .then(() => navigate(form.getAttribute('action')))
+          .catch((error) => alert(error))
+      }
+
+
+    return (
+        <form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Name:
+            <br />
+            <input type="text" name="name" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email:
+            <br />
+            <input type="email" name="email" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Leave a Message:
+            <br />
+            <textarea name="message" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    )
+}
+
+export default Form
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from '../layouts/layout'
+import Form from '../components/Form'
 
 const contact = () => {
     return (
         <main>
             <Layout>
-                
+                <h1>Contact me:</h1>
+                <Form />
             </Layout>
         </main>
     )
