86c60264c83e4d78171ff82f1cc9c7dbe3c05807
Reuben T Allik <reubenallik@gmail.com>
2021-05-14 13:59:05 -0400

    responsive nav and hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,19 +2,18 @@ import React from 'react'
 import { Link, graphql } from 'gatsby';
 
 
-const myName = 'Hi, my name is Reuben Allik'
-
-
 const Hero = () => {
     return (
         <section className='section hero'>
-            <h3 className='my-name'>{myName}</h3>
+            <h3 className='my-name'>
+                Hi<span className='opposite-b'>,</span> my name is Reuben Allik<span className='opposite-b'>.</span>
+            </h3>
             <h2 className='what-i-do'>
                 I build
-                <strong className='emp'> fast</strong>,
-                <strong className='emp'> responsive</strong>, &#38; 
+                <strong className='emp'> fast</strong><span className='opposite-w'>,</span>
+                <strong className='emp'> responsive</strong><span className='opposite-w'> &#38;</span> 
                 <strong className='emp'> accessible </strong> 
-                interfaces for the web
+                interfaces for the web<span className='opposite-w'>.</span>
             </h2>
             <button className='my-work-btn'>
                 <Link to='/projects'>My work</Link>
