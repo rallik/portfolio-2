9de18d4d72ffe0a510fdaa67faa34306cddeb8d6
Reuben T Allik <reubenallik@gmail.com>
2021-05-18 14:33:04 -0400

    sticky about

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import { Link, graphql } from 'gatsby';
 import Tools from '../components/Tools';
+import StickyHeader from '../components/StickyHeaders'
+import about from '../pages/about';
 
 const About = () => {
+    const { aboutRef, isSticky } = StickyHeader();
+    console.log(isSticky)
+    const handleScroll = (e) => {
+        console.log(aboutRef.current.getBoundingClientRect())
+    }
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    })
     return (
         <section className='section about'>
-            <h2 className='about-header'>About</h2>
-            <p className='about-text'>
+            <h2 className={isSticky ? 'about-header stuck' : 'about-header unstuck'}
+            >
+                About
+            </h2>
+            <p className='about-text' ref={aboutRef}>
                 I'm a Boston-based Front-end developer,
                 currently seeking opportunities in the field.
                 In my free time I love taking photos, kayaking,
---
+++ src/components/StickyHeaders.js
@@ -0,0 +1,62 @@
+import { useState, useEffect, useRef, useCallback } from "react";
+
+const StickyHeader = (defaultSticky = false) => {
+    const [isSticky, setIsSticky] = useState(defaultSticky);
+    const aboutRef = useRef(null);
+
+    function getWindowDimensions() {
+        const { innerWidth: width, innerHeight: height } = window;
+        return {
+            width,
+            height
+        };
+    }
+    
+    function useWindowDimensions() {
+        const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+    
+        useEffect(() => {
+            function handleResize() {
+                setWindowDimensions(getWindowDimensions());
+            }
+    
+            window.addEventListener('resize', handleResize);
+            return () => window.removeEventListener('resize', handleResize);
+        }, []);
+    
+        return windowDimensions;
+    }
+
+    const topBound = useWindowDimensions().height + 95;
+
+    const bottomBound = useWindowDimensions().height - 45;
+
+    console.log(topBound, bottomBound);
+    
+
+    const toggleSticky = useCallback(
+        ({ top, bottom }, topBound, bottomBound) => {
+        if (top <= topBound && top > bottomBound) {
+            !isSticky && setIsSticky(true);
+        } else {
+            isSticky && setIsSticky(false);
+        }
+        },
+        [isSticky]
+    );
+
+    useEffect(() => {
+        const handleScroll = () => {
+            toggleSticky(aboutRef.current.getBoundingClientRect(), topBound, bottomBound);
+        };
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+        window.removeEventListener("scroll", handleScroll);
+        };
+    }, [toggleSticky]);
+
+    return { aboutRef, isSticky };
+    };
+
+export default StickyHeader;
