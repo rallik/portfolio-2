019cf8c672dace6db594671ed2ff5e4bbaa1395c
Reuben T Allik <reubenallik@gmail.com>
2021-06-04 18:20:41 -0400

    filtering after query

---
+++ src/components/ProjectPics.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+
+const ProjectPics = ({ pics }) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProjectPics
--- src/pages/projects/{ProjectsJson.id}.js
+++ src/pages/projects/{ProjectsJson.id}.js
@@ -1,18 +1,56 @@
 import React from 'react'
+import { graphql, Link } from 'gatsby';
 import Layout from '../../layouts/layout'
 
 const ProjectTemplate = (props) => {
-    console.log(props)
-    const { location } = props;
+    const { location, data, params: { id } } = props
+    const pictures = data.allFile.pictures;
+    const project = data.projectsJson;
+    const filteredpics = pictures.filter((pic) => pic.name.includes(id))
+    console.log(filteredpics)
+    console.log(project)
+
     return (
         <main>
             <Layout location={location}>
                 <section className='section'>
-                    <h2>project template</h2>
+                <button className='project-card-details-btn'>
+                    <Link to='/projects'>Back to Projects</Link>
+                </button>
+                    <h2 style={{ color: "white" }}>{project.title}</h2>
+                    
                 </section>
             </Layout>
         </main>
     )
 }
+
+export const query = graphql`
+query getSingleProject($id: String) {
+    projectsJson(id: {eq: $id}) {
+      id
+      title
+      subtitle
+      otherimg {
+        alt
+        img
+      }
+      tooltags {
+        tag
+      }
+      description {
+        p
+      }
+      nextsteps
+    }
+    allFile(filter: {name: {ne: "headshot"}, extension: {in: ["jpg", "png"]}}) {
+        pictures: nodes {
+          name
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+  }`
 
 export default ProjectTemplate
