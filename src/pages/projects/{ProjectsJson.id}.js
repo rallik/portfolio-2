b79ebd382d29f3979f5cca7b47abc7d14051130e
Reuben T Allik <reubenallik@gmail.com>
2021-06-21 17:45:46 -0400

    proj pages in progress

--- src/pages/projects/{ProjectsJson.id}.js
+++ src/pages/projects/{ProjectsJson.id}.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { graphql, Link } from 'gatsby';
 import Layout from '../../layouts/layout'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 const ProjectTemplate = (props) => {
     const { location, data, params: { id } } = props
@@ -13,12 +14,32 @@ const ProjectTemplate = (props) => {
     return (
         <main>
             <Layout location={location}>
-                <section className='section'>
-                <button className='project-card-details-btn'>
+                <section className='section individual-proj'>
+                <h1 className='heading-t individual-proj-head'>{project.title}</h1>
+                <h2 className='individual-proj-sub'>{project.subtitle}</h2>
+                <ul className='proj-img-gallery'>
+                {
+                  filteredpics.map((pic) => {
+                    const picdata = getImage(pic)
+                    return (
+                      <li key={pic.name} className='proj-img-gallery-li'>
+                        <GatsbyImage className='proj-img' image={picdata} alt={pic.name} objectFit="contain"></GatsbyImage>
+                      </li>
+                    );
+                      })
+                }
+                </ul>
+                <div className='proj-desc'>
+                {
+                project.description.map((desc) =>
+                  <p key={new Date().getMilliseconds} className='proj-desc-p'>{desc.p}</p>
+                )
+                }
+                </div>
+
+                <button className='btn individual-proj-btn'>
                     <Link to='/projects'>Back to Projects</Link>
                 </button>
-                    <h2 style={{ color: "white" }}>{project.title}</h2>
-                    
                 </section>
             </Layout>
         </main>
