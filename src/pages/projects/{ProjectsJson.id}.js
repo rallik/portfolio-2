6f312446abf58983ac0dec29166dfafa004579c6
Reuben T Allik <reubenallik@gmail.com>
2021-08-05 17:20:38 -0400

    new back to proj buttons

--- src/pages/projects/{ProjectsJson.id}.js
+++ src/pages/projects/{ProjectsJson.id}.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { graphql, Link } from 'gatsby';
 import Layout from '../../layouts/layout'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import Back from '../../assets/svg/back.svg'
 
 const ProjectTemplate = (props) => {
     const { location, data, params: { id } } = props
@@ -12,34 +13,49 @@ const ProjectTemplate = (props) => {
     console.log(project)
 
     return (
-        <main>
-        <Layout location={location} title={`${project.title}: by Reuben Allik`} description={project.subtitle}>
+      <main className={`${project.id}-page`}>
+            <Layout location={location} title={`${project.title}: by Reuben Allik`} description={project.subtitle}>
                 <section className='section individual-proj'>
-                <h1 className='heading-t individual-proj-head'>{project.title}</h1>
-                <h2 className='individual-proj-sub'>{project.subtitle}</h2>
-                <ul className='proj-img-gallery'>
-                {
-                  filteredpics.map((pic) => {
-                    const picdata = getImage(pic)
-                    return (
-                      <li key={pic.name} className='proj-img-gallery-li'>
-                        <GatsbyImage className='proj-img' image={picdata} alt={pic.name} objectFit="contain"></GatsbyImage>
-                      </li>
-                    );
-                      })
-                }
-                </ul>
-                <div className='proj-desc'>
-                {
-                project.description.map((desc) =>
-                  <p key={new Date().getMilliseconds} className='proj-desc-p'>{desc.p}</p>
-                )
-                }
-                </div>
-
-                <button className='btn individual-proj-btn'>
-                    <Link to='/projects'>Back to Projects</Link>
-                </button>
+                    <div className='individual-proj-head-wrapper'>
+                        <button className='btn individual-proj-btn'>
+                            <Link to='/projects'>
+                                <Back/>
+                            </Link>
+                        </button>
+                        <h1 className='heading-t individual-proj-head'>{project.title}</h1>
+                    </div>
+                    <h2 className='individual-proj-sub'>{project.subtitle}</h2>
+                    <ul className='proj-img-gallery'>
+                        {
+                          filteredpics.map((pic) => {
+                            const picdata = getImage(pic)
+                            return (
+                              <li key={pic.name} className='proj-img-gallery-li'>
+                                <GatsbyImage className='proj-img' image={picdata} alt={pic.name} objectFit="contain"></GatsbyImage>
+                              </li>
+                            );
+                              })
+                        }
+                    </ul>
+                    <ul className="tools-used">
+                        {
+                          project.tooltags.map((tool) => {
+                            return (
+                              <li className="tools-used-li" key={tool.tag}>{tool.tag}</li>
+                            );         
+                          })
+                        }
+                    </ul>
+                    <div className='proj-desc'>
+                        {
+                        project.description.map((desc) =>
+                          <p key={new Date().getMilliseconds} className='proj-desc-p'>{desc.p}</p>
+                        )
+                        }
+                    </div>
+                    <div className='next-steps'>
+                        <p>Next steps: {project.nextsteps}</p>
+                    </div>
                 </section>
             </Layout>
         </main>
